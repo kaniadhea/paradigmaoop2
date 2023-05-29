5a7fc7a1d54da83850172fdd260992e6aefd0d8c
kaniadhea <kaniadhea7@gmail.com>
2023-05-29 15:07:05 +0700

    public

---
+++ multipewarisan_/multipewarisan_.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class orang {
+public:
+	string nama;
+
+	orang(string pNama) :
+		nama(pNama) {
+		cout << "orang dibuat\n" << endl;
+	}
+	~orang() {
+		cout << "orang dihapus\n" << endl;
+	}
+};
+
+class manusia {
+public: 
+	string jeniskelamin;
+
+	manusia(string pJenisKelamin) :
+		jeniskelamin(pJenisKelamin) {
+		cout << "manusia dibuat\n" << endl;
+	}
+	~manusia() {
+		cout << "manusia dihapus\n" << endl;
+	}
+};
+
+class pelajar : private manusia, public orang {
+public: 
+	string sekolah;
+
+	pelajar(string pNama, string pJenisKelamin, string pSekolah) :
+		orang(pNama),
+		manusia(pJenisKelamin),
+		sekolah(pSekolah) {
+		cout << "pelajar dibuat\n" << endl;
+	}
+	~pelajar() {
+		cout << "pelajar dihapus\n" << endl;
+	}
+	string perkenalan() {
+		return "Hallo, nama saya" + nama + "dengan jenis kelam" + jeniskelamin
+	}
+};
